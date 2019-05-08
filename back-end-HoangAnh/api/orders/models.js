ed3096dc5b7632e835e5c3d2811221d3de0f3b88
kien2929 <kien2929@gmail.com>
2019-05-08 15:28:52 +0700

    moinhat

--- back-end-HoangAnh/api/orders/models.js
+++ back-end-HoangAnh/api/orders/models.js
@@ -5,16 +5,12 @@ const OrderSchema = new mongoose.Schema({
   phone: Number,
   address:String,
   production:[],
-  total:Number
-  // createdAt: {
-  //   type: Date,
-  //   default: Date.now
-  // }
-  // name : 'Nguyen Hoang Anh',
-  //         phone : '0972742128',
-  //         address : 'Thanh Xuan, Ha Noi',
-  //         production : [{name : 'iphone X', quantity : 3, price : 60000000},{name : 'iphone Y', quantity : 3, price : 60000000}],
-  //         total : 6000
+  total:Number,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 const OrderModel = mongoose.model("Order", OrderSchema);
 
